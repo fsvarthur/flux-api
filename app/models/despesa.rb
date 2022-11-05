775e8056a6ea493e6d950a59ab3d73512e9fced6
fsvarthur <fsvarthur@gmail.com>
2022-11-04 21:37:05 -0300

    scope

--- app/models/despesa.rb
+++ app/models/despesa.rb
@@ -4,4 +4,6 @@ class Despesa < ApplicationRecord
     validates :descricao, presence: true, length: { maximum: 50 }, uniqueness: {scope: :data, message: "Despesa ja cadastrada para esse mêss"}
     validates :valor, presence: true, numericality: { greater_than: 0 }
     validates :data, presence: true
+
+    scope :filter_by_descricao, -> (descricao) { where("descricao like ?", "%#{descricao}%") }
 end
--- app/models/receita.rb
+++ app/models/receita.rb
@@ -2,4 +2,6 @@ class Receita < ApplicationRecord
     validates :descricao, presence: true, length: {maximum: 50}, uniqueness: {scope: :data, message:"Receita já cadastrada para este mês"}
     validates :valor, presence: true, numericality: {greater_than: 0}
     validates :data, presence: true
+
+    scope :filter_by_descricao, -> (descricao) { where("descricao like ?", "%#{descricao}%") }
 end
